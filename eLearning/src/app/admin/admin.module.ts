c1d92fe139d650cdf924665a0969a4a529d11fdb
Krishna Sharma <krishnamca100@gmail.com>
2020-09-29 19:26:31 +0530

    commit7

---
+++ eLearning/src/app/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { VerifyComponent } from './verify/verify.component';
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent, VerifyComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
